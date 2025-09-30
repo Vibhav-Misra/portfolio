f5ec3546b9a10e3ef92abab4f54bf4978706996c
Vibhav-Misra <vibhavemail@gmail.com>
2025-09-30 13:27:53 -0400

    Major functional changes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
-["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
-["src/content/projects/edy-ai-coach.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fedy-ai-coach.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/exoplanet-habitability-explorer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fexoplanet-habitability-explorer.mdx&astroContentModuleFlag=true")],
+["src/content/projects/edy-ai-coach.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fedy-ai-coach.mdx&astroContentModuleFlag=true")],
+["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
 		
