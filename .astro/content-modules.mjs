1ffd73a11d8802025bce303b42f9f2f0a0f5431c
Vibhav-Misra <vibhavemail@gmail.com>
2025-09-30 22:49:30 -0400

    describe change

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,7 @@
 
 export default new Map([
-["src/content/projects/exoplanet-habitability-explorer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fexoplanet-habitability-explorer.mdx&astroContentModuleFlag=true")],
+["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/edy-ai-coach.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fedy-ai-coach.mdx&astroContentModuleFlag=true")],
-["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/exoplanet-habitability-explorer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fexoplanet-habitability-explorer.mdx&astroContentModuleFlag=true")],
+["src/content/projects/memory-manager-for-AI-chatbot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmemory-manager-for-AI-chatbot.mdx&astroContentModuleFlag=true")]]);
 		
