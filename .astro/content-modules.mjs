b176fddeb1f06bd3daacb131a130aee6e0fa25b7
Vibhav-Misra <vibhavemail@gmail.com>
2025-10-05 19:40:32 -0400

    Added a new blog post and edited about section

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,8 @@
 
 export default new Map([
+["src/content/projects/ask-vibhav-resume-chatbot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fask-vibhav-resume-chatbot.mdx&astroContentModuleFlag=true")],
+["src/content/projects/exoplanet-habitability-explorer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fexoplanet-habitability-explorer.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/memory-manager-for-AI-chatbot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmemory-manager-for-AI-chatbot.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
-["src/content/projects/exoplanet-habitability-explorer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fexoplanet-habitability-explorer.mdx&astroContentModuleFlag=true")],
-["src/content/projects/ask-vibhav-resume-chatbot.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fask-vibhav-resume-chatbot.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/data-sources.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fdata-sources.mdx&astroContentModuleFlag=true")]]);
 		
